80bf1ab254c917c58e3e4ba2f9fc75e071d8e8ff
Riddhi1436 <riddhi14032006@gmail.com>
2026-04-02 12:59:43 +0530

    Time: 1 ms (99.91%) | Memory: 42.8 MB (98.67%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] stack = new char[s.length()];
+        int top = -1;
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack[++top] = c; // push
+            } else {
+                if (top == -1) return false; // empty
+                char open = stack[top--]; // pop
+                if ((c == ')' && open != '(') ||
+                    (c == '}' && open != '{') ||
+                    (c == ']' && open != '[')) {
+                    return false;
+                }
+            }
+        }  
+        return top == -1;
+    }
+}
