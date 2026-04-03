67e6d33fffd57d66e89bffcd9ad2ce47d58d5fb8
Riddhi1436 <riddhi14032006@gmail.com>
2026-04-03 12:47:02 +0530

    Time: 2 ms (99.72%) | Memory: 47.3 MB (97.50%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -4,21 +4,16 @@ class Solution {
         int[] res = new int[n];
         int[] stack = new int[2 * n]; // manual stack
         int top = -1;
-
         for (int i = 2 * n - 1; i >= 0; i--) {
             int num = nums[i % n];
-
             while (top >= 0 && stack[top] <= num) {
                 top--;
             }
-
             if (i < n) {
                 res[i] = (top == -1) ? -1 : stack[top];
             }
-
             stack[++top] = num;
         }
-
         return res;
     }
 }
